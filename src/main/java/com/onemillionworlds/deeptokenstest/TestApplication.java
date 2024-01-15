55a91e924592f5a9133482a6d4c2be5e0068d853
Richard Tingle <6330028+richardTingle@users.noreply.github.com>
2024-01-15 18:28:53 +0000

    Add ability to scroll through test tokens

--- src/main/java/com/onemillionworlds/deeptokenstest/TestApplication.java
+++ src/main/java/com/onemillionworlds/deeptokenstest/TestApplication.java
@@ -1,6 +1,9 @@
 package com.onemillionworlds.deeptokenstest;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.light.AmbientLight;
 import com.jme3.light.PointLight;
 import com.jme3.math.ColorRGBA;
@@ -11,8 +14,19 @@ import com.jme3.scene.Spatial;
 import com.jme3.scene.control.AbstractControl;
 import com.jme3.system.AppSettings;
 
+import java.util.List;
+
 public class TestApplication extends SimpleApplication{
 
+    List<String> testList = List.of(
+            "/Models/examples/weatherIcon.j3o",
+            "/Models/examples/problematicTripleHole.j3o", //because the two holes both branch off the same point this one tends to go wrong
+            "/Models/examples/arch.j3o",
+            "/Models/examples/difficultHolesAndIslands.j3o"
+    );
+
+
+    int currentlyDisplaying = 0;
 
     public static void main(String[] args){
         AppSettings settings = new AppSettings(true);
@@ -30,7 +44,52 @@ public class TestApplication extends SimpleApplication{
         cam.setLocation(new Vector3f(0, 2, 10));
         cam.lookAt(new Vector3f(0, 0, 0), Vector3f.UNIT_Y);
 
-        Spatial deepToken = assetManager.loadModel("/Models/weatherIcon.j3o");
+        setUpToDisplayIndex(currentlyDisplaying);
+
+        Vector3f lightPosition = new Vector3f(5, 10, 10);
+        PointLight pointLight = new PointLight();
+
+        pointLight.setPosition(lightPosition);
+        rootNode.addLight(pointLight);
+
+        AmbientLight ambientLight = new AmbientLight(new ColorRGBA(0.3f, 0.3f, 0.3f, 1));
+        rootNode.addLight(ambientLight);
+
+        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_Q));
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_E));
+
+         ActionListener actionListener = new ActionListener() {
+            @Override
+            public void onAction(String name, boolean isPressed, float tpf) {
+                if (isPressed) {
+                    if (name.equals("Left")) {
+                        currentlyDisplaying++;
+                    } else if (name.equals("Right")) {
+                        currentlyDisplaying--;
+                    }
+                    if (currentlyDisplaying < 0) {
+                        currentlyDisplaying += testList.size();
+                    }
+                    currentlyDisplaying = currentlyDisplaying% testList.size();
+                    setUpToDisplayIndex(currentlyDisplaying);
+                }
+            }
+        };
+
+        inputManager.addListener(actionListener, "Left", "Right");
+    }
+
+
+
+    @Override
+    public void simpleUpdate(float tpf){
+        super.simpleUpdate(tpf);
+    }
+
+    private void setUpToDisplayIndex(int index){
+        rootNode.detachAllChildren();
+
+        Spatial deepToken = assetManager.loadModel(testList.get(index));
 
         rootNode.attachChild(deepToken);
 
@@ -45,16 +104,6 @@ public class TestApplication extends SimpleApplication{
             @Override
             protected void controlRender(RenderManager rm, ViewPort vp){}
         });
-
-
-        Vector3f lightPosition = new Vector3f(5, 10, 10);
-        PointLight pointLight = new PointLight();
-
-        pointLight.setPosition(lightPosition);
-        rootNode.addLight(pointLight);
-
-        AmbientLight ambientLight = new AmbientLight(new ColorRGBA(0.3f, 0.3f, 0.3f, 1));
-        rootNode.addLight(ambientLight);
     }
 
 }
